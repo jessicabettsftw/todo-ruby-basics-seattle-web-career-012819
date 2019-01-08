39dc24d10c25d3ac5bcfa2959ef90b6a9491183c
JESSICA A BETTS <jessicaannbettsis@gmail.com>
2019-01-08 04:51:52 +0000

    Automatically backed up by Learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -17,13 +17,13 @@ def greeting(input1, input2)
 end
 
 def return_a_value
-
+  return "Nice"
 end
 
 def last_evaluated_value
-
+  return "expert"
 end
 
-def pizza_party
-
+def pizza_party(choice = "cheese")
+  return choice
 end
